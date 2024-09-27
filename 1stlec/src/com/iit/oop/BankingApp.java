291a57b3126cc4de50ecfac73efbd107ccbdcce4
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-09-27 09:52:41 +0530

    Update BankingApp.java

--- 1stlec/src/com/iit/oop/BankingApp.java
+++ 1stlec/src/com/iit/oop/BankingApp.java
@@ -1,5 +1,10 @@
 package com.iit.oop;
 
 public class BankingApp {
-    BankAccount Ashen=new BankAccount(0,0001);
+    public static void main(String[] args) {
+        BankAccount Ashen=new BankAccount(0,0001);
+        Ashen.deposit(56200);
+        System.out.println(Ashen.getBalance());
+    }
+
 }
