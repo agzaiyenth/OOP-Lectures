01f82b662925f2d6b988a8881f50b30e20111f99
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-10-02 11:43:06 +0530

    Tut 1

--- 1stlec/src/com/iit/oop/BankingApp.java
+++ 1stlec/src/com/iit/oop/BankingApp.java
@@ -1,10 +1,93 @@
 package com.iit.oop;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BankingApp {
     public static void main(String[] args) {
-        BankAccount Ashen=new BankAccount(0,0001);
-        Ashen.deposit(56200);
-        System.out.println(Ashen.getBalance());
+
+        Scanner scan = new Scanner(System.in);
+        int[] arr = new int[6];
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Enter an integer: ");
+            arr[i] = scan.nextInt();
+
+        }
+        scan.close();
+        for (int i = 0; i < 6; i++) {
+            System.out.println(arr[i]);
+        }
+
+        int below = 0,pass=0,two=0,one=0,first=0;
+        for (int i = 0; i < 6; i++) {
+            if (arr[i]<40){
+                below+=1;
+            } else if (arr[i]>40 && arr[i]<50) {
+                pass+=1;
+            }else if (arr[i]>49 && arr[i]<60) {
+                two+=1;
+            }else if (arr[i]>59 && arr[i]<70) {
+                one+=1;
+            }else{
+                first+=1;
+            }
+        }
+        System.out.printf("""
+                below: %d
+                Pass: %d
+                2-2: %d
+                2-1: %d
+                First: %d
+                """,below,pass,two,one,first);
+        System.out.println("Average :"+average(arr));
+        System.out.println("Maximum :"+findMax(arr));
+        System.out.println("Minimum :"+findMin(arr));
+        int[] sortedarr=bubbleSort(arr);
+        System.out.print("Sorted Array : ");
+        for(int i=0;i<sortedarr.length;i++){
+            System.out.print(sortedarr[i]+" ");
+        }
+
+
+    }
+    public static double average(int[] arr){
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum/arr.length;
+    }
+    public static int findMax(int[] arr){
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static int findMin(int[] arr){
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+    public static int[] bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
     }
 
 }
