9efd77f71fa91cc981dcf8b60df4f8918630c430
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-10-09 11:48:38 +0530

    edits

---
+++ 1stlec/src/com/iit/oop/Movie.java
@@ -0,0 +1,39 @@
+package com.iit.oop;
+
+public class Movie {
+    private String title;
+    private String category;
+    private Director director;
+    private int numAward;
+
+    public Movie(String title,String category,Director director) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setNumAward(int numAward) {
+        this.numAward = numAward;
+    }
+    public int getNumAward() {
+        return numAward;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie "+title+"\nCategory "+category+"\nNo of Awards "+numAward;
+    }
+    
+}
